657acf9ea13cc75bef4192841ada14e84ae9fb88
g12-hf <b5j8@ece.ubc.ca>
2013-10-24 14:49:16 -0700

    add better table

--- Assignment3/Philosophers/anim.py
+++ Assignment3/Philosophers/anim.py
@@ -8,8 +8,9 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from panda3d.core import TextureStage, Texture
 
-from get_constants import get_constants
+from .get_constants import get_constants
 
 # XXX obviously change this before submitting
 ASSETS_DIR = "/home/hamza/Dropbox/_SELF/Panda3D/"
@@ -27,21 +28,30 @@ class PhilosophicalDiners(ShowBase):
 
         self.models = {}
         # Load environment (really just a plane)
-        self._load_model("house", scale=[25, 25, 25])
+        self._load_model("house", scale=[25, 25, 25], pos=[0, 0, -10])
         # Load the "table"
-        self._load_model("table", scale=[7, 7, 7], pos=[0, 0, 1])
+        self._load_model("round_table", scale=[27, 27, 14], pos=[0, 0, 0])
         # Load and place forks
         self._load_forks()
 
         # Apply textures to models
-        self.brown_tex = self.loader.loadTexture(ASSETS_DIR + "brown.png")
-        self.models["table"].setTexture(self.brown_tex)
+        self.wood_tex = self.loader.loadTexture(
+            ASSETS_DIR + "derevo_mebel.jpg")
+        self.models["round_table"].setTexture(self.wood_tex)
+        self.models["round_table"].setTexScale(
+            TextureStage.getDefault(), 0.005, 0.005)
+
         self.black_tex = self.loader.loadTexture(ASSETS_DIR + "black.png")
         self.models["house"].setTexture(self.black_tex)
 
+        # Tasks
         self.taskMgr.add(self.spin_camera, "spin_camera")
 
     def _load_model(self, name, **kwargs):
+        """Load name.egg as name as name in self.models
+
+        Also sets scale, pos, and/or hpr as per kwargs
+        """
         self.models[name] = self.loader.loadModel(ASSETS_DIR + name)
         self.models[name].reparentTo(self.render)
         if kwargs.get("scale"):
@@ -64,15 +74,15 @@ class PhilosophicalDiners(ShowBase):
             x, y, angle = self._get_fork_coord(i)
             self.forks[i] = self.loader.loadModel(ASSETS_DIR + "fork")
             self.forks[i].reparentTo(self.render)
-            self.forks[i].setScale(5, 5, 5)
-            self.forks[i].setPos(x, y, 1.2)
+            self.forks[i].setScale(3, 3, 3)
+            self.forks[i].setPos(x, y, 2.3)
             self.forks[i].setH(self.forks[i], angle * 180 / pi + 90)
 
     def _get_fork_coord(self, num):
         """Do mathemagics to get cartesian coordinates of a fork by its number
         on the table
         """
-        diameter = pi * 3.4  # This number is a guess
+        diameter = pi * 3.6  # This number is a guess
                           # Take care to change this if table-size changes
         angle = num * 2 * pi / self.p_constants["NPHILOSOPHERS"]
         return (diameter / 2 * cos(angle), diameter / 2 * sin(angle), angle)
