898df15f918601a31c149417e165279a54fd202f
g12-hf <b5j8@ece.ubc.ca>
2013-10-24 00:16:50 -0700

    Add initial Panda3D code

--- Assignment3/Philosophers/anim.py
+++ Assignment3/Philosophers/anim.py
@@ -1,25 +1,62 @@
 import zmq
 import subprocess
 
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
 
-def create_socket_context():
-    """Creates context, socket and returns them"""
-    # Find upper bound on ACTIME from constants and set timeout to double that
-    with open("constants.h", "r") as f:
-        lines = f.read().split("\n")
-        for line in lines:
-            if "ACTIME" in line:
-                timeout = int(2000 * float(line.split(" ")[-1].strip("s")))
-                break
-        else:
-            raise Exception("Did not find ACTIME bound in constants.h")
+ASSETS_DIR = "/home/hamza/Dropbox/_SELF/Panda3D/" # XXX obviously change this before submitting
 
-    context = zmq.Context()  # Create Context
-    socket = context.socket(zmq.REQ)  # Create socket
-    socket.connect("tcp://127.0.0.101:9123")  # Connect to dining philosophers
-    socket.RCVTIMEO = timeout  # Set timeout
 
-    return context, socket
+class PhilosophicalDiners(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.context, self.socket = self._create_socket_context()
+
+        self.environ = self.loader.loadModel("models/environment")
+        self.environ.reparentTo(self.render)
+        self.environ.setScale(0.25, 0.25, 0.25)
+        self.environ.setPos(-8, 42, 0)
+
+        self.table_model = self.loader.loadModel(ASSETS_DIR + "table")
+        self.table_model.reparentTo(self.render)
+        self.table_model.setScale(5, 5, 5)
+        self.table_model.setPos(0, 0, 10)
+
+        self.fork_model = self.loader.loadModel(ASSETS_DIR + "fork")
+        self.fork_model.reparentTo(self.render)
+        self.fork_model.setScale(5, 5, 5)
+        self.fork_model.setPos(0, 0, 10)
+
+
+        self.brown_tex = self.loader.loadTexture(ASSETS_DIR + "brown.png")
+        self.table_model.setTexture(self.brown_tex)
+        # self.silver_tex = self.loader.loadTexture(ASSETS_DIR + "silver.png")
+        # self.table_model.setTexture(self.silver_tex)
+
+    def _create_socket_context(self):
+        """Creates context, socket and returns them"""
+        # Find upper bound on ACTIME from constants and set timeout to double
+        # that
+        with open("constants.h", "r") as f:
+            lines = f.read().split("\n")
+            for line in lines:
+                if "ACTIME" in line:
+                    timeout = int(2000 * float(line.split(" ")[-1].strip("s")))
+                    break
+            else:
+                raise Exception("Did not find ACTIME bound in constants.h")
+
+        context = zmq.Context()  # Create Context
+        socket = context.socket(zmq.REQ)  # Create socket
+        # Connect to dining philosophers
+        socket.connect("tcp://127.0.0.101:9123")
+        socket.RCVTIMEO = timeout  # Set timeout
+
+        return context, socket
 
 
 def main():
@@ -42,4 +79,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    app = PhilosophicalDiners()
+    app.run()
