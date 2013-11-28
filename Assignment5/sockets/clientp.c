a25ab531fa622faf12a21972cdaa1cdcf3a2ac6b
Hamza Faran <hamzafaran@hotmail.com>
2013-11-28 00:47:01 -0800

    Added reply func, but broken -_-

--- Assignment5/sockets/clientp.c
+++ Assignment5/sockets/clientp.c
@@ -1,17 +1,21 @@
 #include <windows.h>
 #include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
 
 #define SERV_PIPE "\\\\.\\pipe\\serv_pipe"
 #define PIPE_BUFFER_SIZE 1024
 #define MESG_BUFFER_SIZE 80
-#define TIMEOUT 4000
+#define TIMEOUT 2000
 
 int main()
 {
     HANDLE h;  //Handle for the pipe
     DWORD dwBytesRead;  //Number of bytes read from the pipe
     DWORD dwWritten;  //Number of bytes written to the pipe
-    char buf[MESG_BUFFER_SIZE];  //Buffer to store stream from pipe in
+    TCHAR recv_buf[MESG_BUFFER_SIZE];  //Buffer to store stream from pipe in
+    LPTSTR send_buf = TEXT("Hello! I am a client!");  //Buffer to store message to write
+
 
     if (!WaitNamedPipe(SERV_PIPE, NMPWAIT_WAIT_FOREVER))
     {
@@ -20,11 +24,11 @@ int main()
     }
     h = CreateFile(
             SERV_PIPE,
-            GENERIC_WRITE,
+            GENERIC_WRITE | GENERIC_READ,
             0,
             NULL,
             OPEN_EXISTING,
-            FILE_ATTRIBUTE_NORMAL,
+            0,
             NULL
         );
     if (h == INVALID_HANDLE_VALUE)
@@ -32,13 +36,35 @@ int main()
         printf("Error code %d happened during CreateFile. :(\n", GetLastError());
         return 1;
     }
+    printf("Successfully connected to pipe. :)\n"); fflush(stdout);
 
-    sprintf(buf, "Hello! I am a client!");
-    if (!WriteFile(h, buf, MESG_BUFFER_SIZE, &dwWritten, NULL))
+    sprintf(send_buf, "Hello! I am a client!");
+    if (!WriteFile(
+                h,
+                send_buf,
+                (lstrlen(send_buf) + 1)*sizeof(TCHAR),
+                &dwWritten,
+                NULL))
     {
-        printf("Failed to write to the pipe. :(\n");
+        printf("Failed to write to the pipe. :( ERR %d\n", GetLastError()); fflush(stdout);
         return 1;
     }
+    FlushFileBuffers(h);
+    printf("Successfully wrote message to the server.\n"); fflush(stdout);
+
+    // While the server sends data, receive it
+    while (1)
+    {
+        if (!ReadFile(
+                    h,
+                    recv_buf,
+                    MESG_BUFFER_SIZE * sizeof(TCHAR),
+                    &dwBytesRead,
+                    NULL))
+            break;
+    }
+    //Print received message
+    printf("Message from server: %s\n", recv_buf); fflush(stdout);
 
     //Close the handle and return
     CloseHandle(h);
--- Assignment5/sockets/serverp.c
+++ Assignment5/sockets/serverp.c
@@ -1,45 +1,70 @@
 #include <windows.h>
 #include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
 
 #define SERV_PIPE "\\\\.\\pipe\\serv_pipe"
 #define PIPE_BUFFER_SIZE 1024
 #define MESG_BUFFER_SIZE 80
-#define TIMEOUT 4000
+#define TIMEOUT 2000
 
 int main()
 {
     HANDLE h;  //Handle for the pipe
     DWORD dwBytesRead;  //Number of bytes read from the pipe
-    char buf[MESG_BUFFER_SIZE];  //Buffer to store stream from pipe in
+    DWORD dwWritten;  //Number of bytes written to the pipe
+    TCHAR recv_buf[MESG_BUFFER_SIZE];  //Buffer to store stream from pipe in
+    LPTSTR send_buf = TEXT("Hello! I am a server!");  //Buffer to store message to write
 
     // Create a named pipe
     h = CreateNamedPipe(
-                   SERV_PIPE,  //Name of the pipe as defined above
-                   PIPE_ACCESS_DUPLEX | WRITE_DAC,  //OpenMode: bidirectional
-                   //RW stream of bytes, blocking
-                   PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
-                   1,  //Instances of the pipe that can be created
-                   PIPE_BUFFER_SIZE,  //Input buffer size
-                   PIPE_BUFFER_SIZE,  //Output buffer size
-                   TIMEOUT,  //Time to wait before timing out,
-                   NULL  //Optional security attributes
-               );
+            SERV_PIPE,  //Name of the pipe as defined above
+            PIPE_ACCESS_DUPLEX,  //OpenMode: bidirectional
+            //RW stream of bytes, blocking
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,  //Instances of the pipe that can be created
+            PIPE_BUFFER_SIZE,  //Input buffer size
+            PIPE_BUFFER_SIZE,  //Output buffer size
+            TIMEOUT,  //Time to wait before timing out,
+            NULL  //Optional security attributes
+        );
     // In case of an invalid handle, die
     if (h == INVALID_HANDLE_VALUE)
     {
-        printf("Pipe creation failed\n");
+        printf("Pipe creation failed, ERR %d\n", GetLastError());
         return 1;
     }
     // If we got a valid handle, connect to the pipe
     ConnectNamedPipe(h, NULL);
+    printf("Pipe successfully created and connected. :)\n"); fflush(stdout);
     // While the client sends data, receive it
-    while (!0)
-        if (!ReadFile(h, buf, MESG_BUFFER_SIZE, &dwBytesRead, NULL)) break;
+    while (1)
+    {
+        if (!ReadFile(
+                    h,
+                    recv_buf,
+                    MESG_BUFFER_SIZE * sizeof(TCHAR),
+                    &dwBytesRead,
+                    NULL))
+            break;
+    }
     //Print received message
-    printf("Message from client: %s\n", buf);
+    printf("Message from client: %s\n", recv_buf); fflush(stdout);
 
+    //sprintf(send_buf, "Hello! I am a server!");
+    if (!WriteFile(
+                h,
+                send_buf,
+                (lstrlen(send_buf) + 1)*sizeof(TCHAR),
+                &dwWritten,
+                NULL))
+    {
+        printf("Failed to write to the pipe. :( ERR %d\n", GetLastError()); fflush(stdout);
+        return 1;
+    }
 
     //Disconnect pipe, close handle
+    FlushFileBuffers(h);
     DisconnectNamedPipe(h);
     CloseHandle(h);
     return 0;
